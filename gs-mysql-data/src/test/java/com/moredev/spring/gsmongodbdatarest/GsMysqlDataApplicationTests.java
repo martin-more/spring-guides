170b53cb3d3acf94652f277ad5b0604cd7517528
Martin More <martinmore52@gmail.com>
2018-09-14 02:03:23 -0500

    gs-mysql-data pom.xml: Defining necessary dependencies (JPA Data, Mysql Connector, Starter Test, Starter Web)

---
+++ gs-mysql-data/src/test/java/com/moredev/spring/gsmongodbdatarest/GsMysqlDataApplicationTests.java
@@ -0,0 +1,16 @@
+package com.moredev.spring.gsmongodbdatarest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GsMysqlDataApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
